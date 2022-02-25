32bad888495f932a076be092cf119c1ff05c1815
thdwoqor <celebarte01@gmail.com>
2022-02-25 17:11:38 +0900

    feat: add driver

---
+++ util/driver.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")  # headless 모드 설정
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+chrome_options.page_load_strategy = "none"
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
