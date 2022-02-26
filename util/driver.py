6f4980c17fbfb2b1767c8545661d4b9d92a280e5
thdwoqor <celebarte01@gmail.com>
2022-02-26 19:37:53 +0900

    docs: update driver

--- util/driver.py
+++ util/driver.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("headless")  # headless 모드 설정
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-chrome_options.page_load_strategy = "none"
+options = webdriver.ChromeOptions()
+options.add_argument("headless")  # headless 모드 설정
+options.add_argument("window-size=1920x1080")
+options.add_argument("--start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.page_load_strategy = "none"
 
-driver = webdriver.Chrome(service="chromedriver", options=chrome_options)
+driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
